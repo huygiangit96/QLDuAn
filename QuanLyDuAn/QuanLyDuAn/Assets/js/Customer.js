ea734f0dbce807521eeb330dcb6b62d643905b89
Diagru25 <31183495+Diagru25@users.noreply.github.com>
2018-04-06 14:59:43 +0700

    âss

---
+++ QuanLyDuAn/QuanLyDuAn/Assets/js/Customer.js
@@ -0,0 +1,98 @@
+﻿
+// Delete one customer
+$('.delete_cus').click(function () {
+    var id = $(this).data('id');
+    var r = confirm("bạn có chăn chắn muốn xóa không ?");
+    if (r == true) {
+        $.ajax({
+            url: '/Customer/Delete',
+            data: { id: id },
+            type: 'GET',
+            datatype: 'json',
+            success: function (data) {
+                if (data == true) {
+                    alert("Xóa thành công");
+                    window.location.href = "/Customer/Index";
+                }
+                else {
+                    alert("Có lỗi trong quá trình xóa");
+                }
+            },
+            error: function (err) {
+                alert("Không thành công");
+            }
+        })
+    }
+});
+
+//Insert one customer
+$('#Insert_Cus').click(function () {
+    var id = $('#Cus_code').val();
+    var ten = $('#Cus_name').val();
+    var diachi = $('#Cus_address').val();
+    var sotk = $('#Cus_bank').val();
+    var phone = $('#Cus_phone').val();
+
+    $.ajax({
+        url: '/Customer/Insert',
+        data: { code: id, name: ten, address: diachi, bank: sotk, phone: phone },
+        type: 'POST',
+        datatype: 'json',
+        success: function (data) {
+            if (data == true) {
+                alert("Thêm thành công");
+            }
+            window.location.href = "/Customer/Index";
+        },
+        error: function () {
+            alert("Đã xảy ra lỗi");
+        }
+    })
+});
+
+// Update one customer
+$('#Edit_Cus').click(function () {
+    var id = $('#Cus_code').val();
+    var ten = $('#Cus_name').val();
+    var diachi = $('#Cus_address').val();
+    var sotk = $('#Cus_bank').val();
+    var phone = $('#Cus_phone').val();
+
+    $.ajax({
+        url: '/Customer/Edit',
+        data: { code: id, name: ten, address: diachi, bank: sotk, phone: phone },
+        type: 'POST',
+        datatype: 'json',
+        success: function (data) {
+            if (data == true) {
+                alert("Cập nhật thành công");
+            }
+            window.location.href = "/Customer/Index";
+        },
+        error: function () {
+            alert("Đã xảy ra lỗi");
+        }
+    })
+});
+
+// show one customer on modal
+$('.btn_edit_cus').click(function () {
+    var id = $(this).data('id');
+    $.ajax({
+        url: '/Customer/Get',
+        data: { id: id },
+        type: 'GET',
+        datatype: 'json',
+        success: function (data) {
+            $('#Cus_code_E').val(id);
+            $('#Cus_name_E').val(data.Ten);
+            $('#Cus_address_E').val(data.DiaChi);
+            $('#Cus_bank_E').val(data.SoTK);
+            $('#Cus_phone_E').val(data.SoDT);
+            $('#editModal').modal('show');
+        },
+        error: function (err) {
+            alert("Error: " + err.responseText);
+        }
+    })
+});
---
+++ QuanLyDuAn/QuanLyDuAn/Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using Model.DAO;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDuAn.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<KhachHang> model = new List<KhachHang>();
+            model = new KhachHangDAO().ListAll();
+            return View(model);
+        }
+
+        // get current customer show on modal
+        public JsonResult Get(string id)
+        {
+            KhachHang item = new KhachHangDAO().GetByID(id);
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Delete(string id)
+        {
+            bool status = new KhachHangDAO().Delete(id);
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult Insert(string code, string name, string address, string bank, string phone)
+        {
+            KhachHang item = new KhachHang();
+            item.MaKH = code;
+            item.Ten = name;
+            item.DiaChi = address;
+            item.SoTK = bank;
+            item.SoDT = phone;
+            bool result = new KhachHangDAO().Insert(item);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult Edit(string code, string name, string address, string bank, string phone)
+        {
+            KhachHang item = new KhachHang();
+            item.MaKH = code;
+            item.Ten = name;
+            item.DiaChi = address;
+            item.SoTK = bank;
+            item.SoDT = phone;
+            bool result = new KhachHangDAO().Update(item);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
