7b8536474f93411aaef110161fc5216fcf11a9dc
huygiangit96 <32055253+huygiangit96@users.noreply.github.com>
2018-04-11 14:06:26 +0700

    as

---
+++ QuanLyDuAn/Model/DAO/ChiTietLichLamViecDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+
+namespace Model.DAO
+{
+    public class ChiTietLichLamViecDAO
+    {
+        QLDADbContext db = null;
+
+        public ChiTietLichLamViecDAO()
+        {
+            db = new QLDADbContext();
+        }
+        public List<ChiTietLichLamViec> ListAll()
+        {
+            return db.ChiTietLichLamViecs.ToList();
+        }
+    }
+}
--- QuanLyDuAn/QuanLyDuAn/Controllers/TaskController.cs
+++ QuanLyDuAn/QuanLyDuAn/Controllers/TaskController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model.DAO;
 
 namespace QuanLyDuAn.Controllers
 {
@@ -25,7 +26,11 @@ namespace QuanLyDuAn.Controllers
         {
             return View();
         }
-
+        public JsonResult GetCalendar()
+        {
+            var item = new ChiTietLichLamViecDAO().ListAll();
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
         // chấm công
         public ActionResult Time_keeping()
         {
